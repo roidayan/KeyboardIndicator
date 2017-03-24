f2e2f4f856a9ffb0eb74d9cd2a3a4b5f1f5decc3
Roi Dayan <roi.dayan@gmail.com>
2017-03-25 00:17:36 +0300

    Set v1.6.2.0
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- KeyboardIndicator/Properties/AssemblyInfo.cs
+++ KeyboardIndicator/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Roi Dayan")]
 [assembly: AssemblyProduct("Keyboard Indicator")]
-[assembly: AssemblyCopyright("Copyright © 2009-2016 Roi Dayan")]
+[assembly: AssemblyCopyright("Copyright © 2009-2017 Roi Dayan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("")]
